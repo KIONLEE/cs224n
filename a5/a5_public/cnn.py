66a3c06be540ff6d6e453453478e4ecd8e97b41b
keonlee9420 <rjsdlwwkd@kaist.ac.kr>
2020-07-08 21:10:40 +0900

    g done

--- a5/a5_public/cnn.py
+++ a5/a5_public/cnn.py
@@ -12,6 +12,28 @@ class CNN(nn.Module):
     # pass
     # Remember to delete the above 'pass' after your implementation
     ### YOUR CODE HERE for part 1g
+    def __init__(self, e_char, e_word):
+        """ Init CNN.
+
+        @param e_word (int): Output embedding size of target char.
+        @param e_word (int): Output embedding size of target word.
+        """
+        super(CNN, self).__init__()
+        self.conv = nn.Conv1d(in_channels=e_char, out_channels=e_word, , kernel_size=5, padding=1)
+        self.ReLU = nn.ReLU() # 'inplace' default: False
+        self.maxpool = nn.AdaptiveMaxPool1d(output_size=1)
+
+    def forward(self, x_reshaped): # x_reshaped => x_conv_out
+        """ Forward pass of CNN.
+
+        @param x_reshaped (Tensor): tensor after padding and embedding lookup, shape (src_len * batch_size, e_char, m_word)
+        
+        @returns x_conv_out (Tensor): output tensor after highway layer, shape (src_len * batch_size, e_word)
+        """
+        relu = self.ReLU(self.conv(x_reshaped)) # (src_len * b, e_word, m_word - kernel_size + 1)
+        x_conv_out = self.maxpool(relu) # (src_len * b, e_word)
+
+        return x_conv_out
 
     ### END YOUR CODE
 
